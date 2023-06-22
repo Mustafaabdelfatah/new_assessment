b785b5ef02377b20ff82f0d039538638aaa0efc7
Mustafa Salama <mustafa.salama2608@gmail.com>
2023-06-22 16:48:23 +0300

    close file

---
+++ resources/views/dashboard/pages/close.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Close Assessment</title>
+    <style>
+        .alert {
+            background-color: #f8f9fa;
+            border: 2px solid #343a40;
+            border-radius: 6px;
+            padding: 20px;
+            text-align: center;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            max-width: 400px;
+            margin: 0 auto;
+        }
+
+        .alert h2 {
+            margin: 0;
+            font-size: 24px;
+            color: #343a40;
+        }
+
+        .alert p {
+            margin: 10px 0;
+            font-size: 16px;
+            color: #6c757d;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <div class="alert">
+        <h2>Deadline Reached</h2>
+        <p>The deadline for your task has passed.</p>
+        <p>Please take necessary actions accordingly.</p>
+    </div>
+
+</body>
+
+</html>
--- resources/views/dashboard/user-index.blade.php
+++ resources/views/dashboard/user-index.blade.php
@@ -6,7 +6,7 @@
             <!--begin: Pic-->
             <div class="me-7 mb-4">
                 <div class="symbol profile-image-container symbol-100px symbol-lg-160px symbol-fixed position-relative">
-                    <img id="profile-image" src="{{ asset($user_data?->image_path) }}" alt="image"/>
+                    <img id="profile-image" src="{{ asset($user_data?->image_path) }}" alt="image" />
                     <div class="edit-icon" onclick="document.getElementById('profile-image').click();">
                         <i class="fa fa-pencil"></i>
                     </div>
@@ -25,20 +25,20 @@
                         <!--begin::Name-->
                         <div class="d-flex align-items-center mb-2">
                             <a href="#"
-                               class="text-gray-900 text-hover-primary fs-2 fw-bold me-1">{{ $user_data->name }}</a>
+                                class="text-gray-900 text-hover-primary fs-2 fw-bold me-1">{{ $user_data->name }}</a>
                             <a href="#">
                                 {{--
                                 <!--begin::Svg Icon | path: icons/duotune/general/gen026.svg-->
                                 --}}
                                 <span class="svg-icon svg-icon-1 svg-icon-primary">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px"
-                                         viewBox="0 0 24 24">
+                                        viewBox="0 0 24 24">
                                         <path
                                             d="M10.0813 3.7242C10.8849 2.16438 13.1151 2.16438 13.9187 3.7242V3.7242C14.4016 4.66147 15.4909 5.1127 16.4951 4.79139V4.79139C18.1663 4.25668 19.7433 5.83365 19.2086 7.50485V7.50485C18.8873 8.50905 19.3385 9.59842 20.2758 10.0813V10.0813C21.8356 10.8849 21.8356 13.1151 20.2758 13.9187V13.9187C19.3385 14.4016 18.8873 15.491 19.2086 16.4951V16.4951C19.7433 18.1663 18.1663 19.7433 16.4951 19.2086V19.2086C15.491 18.8873 14.4016 19.3385 13.9187 20.2758V20.2758C13.1151 21.8356 10.8849 21.8356 10.0813 20.2758V20.2758C9.59842 19.3385 8.50905 18.8873 7.50485 19.2086V19.2086C5.83365 19.7433 4.25668 18.1663 4.79139 16.4951V16.4951C5.1127 15.491 4.66147 14.4016 3.7242 13.9187V13.9187C2.16438 13.1151 2.16438 10.8849 3.7242 10.0813V10.0813C4.66147 9.59842 5.1127 8.50905 4.79139 7.50485V7.50485C4.25668 5.83365 5.83365 4.25668 7.50485 4.79139V4.79139C8.50905 5.1127 9.59842 4.66147 10.0813 3.7242V3.7242Z"
-                                            fill="currentColor"/>
+                                            fill="currentColor" />
                                         <path
                                             d="M14.8563 9.1903C15.0606 8.94984 15.3771 8.9385 15.6175 9.14289C15.858 9.34728 15.8229 9.66433 15.6185 9.9048L11.863 14.6558C11.6554 14.9001 11.2876 14.9258 11.048 14.7128L8.47656 12.4271C8.24068 12.2174 8.21944 11.8563 8.42911 11.6204C8.63877 11.3845 8.99996 11.3633 9.23583 11.5729L11.3706 13.4705L14.8563 9.1903Z"
-                                            fill="white"/>
+                                            fill="white" />
                                     </svg>
                                 </span>
                                 <!--end::Svg Icon-->
@@ -50,19 +50,19 @@
                         --}}
                         <div class="d-flex flex-wrap fw-semibold fs-6 mb-4 pe-2">
                             <a href="#"
-                               class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
+                                class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
                                 <!--begin::Svg Icon | path: icons/duotune/communication/com006.svg-->
                                 <span class="svg-icon svg-icon-4 me-1">
                                     <svg width="18" height="18" viewBox="0 0 18 18" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
+                                        xmlns="http://www.w3.org/2000/svg">
                                         <path opacity="0.3"
-                                              d="M16.5 9C16.5 13.125 13.125 16.5 9 16.5C4.875 16.5 1.5 13.125 1.5 9C1.5 4.875 4.875 1.5 9 1.5C13.125 1.5 16.5 4.875 16.5 9Z"
-                                              fill="currentColor"/>
+                                            d="M16.5 9C16.5 13.125 13.125 16.5 9 16.5C4.875 16.5 1.5 13.125 1.5 9C1.5 4.875 4.875 1.5 9 1.5C13.125 1.5 16.5 4.875 16.5 9Z"
+                                            fill="currentColor" />
                                         <path
                                             d="M9 16.5C10.95 16.5 12.75 15.75 14.025 14.55C13.425 12.675 11.4 11.25 9 11.25C6.6 11.25 4.57499 12.675 3.97499 14.55C5.24999 15.75 7.05 16.5 9 16.5Z"
-                                            fill="currentColor"/>
+                                            fill="currentColor" />
                                         <rect x="7" y="6" width="4" height="4" rx="2"
-                                              fill="currentColor"/>
+                                            fill="currentColor" />
                                     </svg>
                                 </span>
                                 <!--end::Svg Icon-->
@@ -73,13 +73,13 @@
                                 <!--begin::Svg Icon | path: icons/duotune/communication/com011.svg-->
                                 <span class="svg-icon svg-icon-4 me-1">
                                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
+                                        xmlns="http://www.w3.org/2000/svg">
                                         <path opacity="0.3"
-                                              d="M21 19H3C2.4 19 2 18.6 2 18V6C2 5.4 2.4 5 3 5H21C21.6 5 22 5.4 22 6V18C22 18.6 21.6 19 21 19Z"
-                                              fill="currentColor"/>
+                                            d="M21 19H3C2.4 19 2 18.6 2 18V6C2 5.4 2.4 5 3 5H21C21.6 5 22 5.4 22 6V18C22 18.6 21.6 19 21 19Z"
+                                            fill="currentColor" />
                                         <path
                                             d="M21 5H2.99999C2.69999 5 2.49999 5.10005 2.29999 5.30005L11.2 13.3C11.7 13.7 12.4 13.7 12.8 13.3L21.7 5.30005C21.5 5.10005 21.3 5 21 5Z"
-                                            fill="currentColor"/>
+                                            fill="currentColor" />
                                     </svg>
                                 </span>
                                 <!--end::Svg Icon-->
@@ -114,8 +114,8 @@
             <div class="mt-4">
                 @foreach ($assessmentsDates as $assessmentsDate)
                     <a class="d-inline-block mx-2 btn btn-secondary "
-                       style="{{ Carbon::parse($month)->format('Y-m') == Carbon::parse($assessmentsDate)->format('Y-m') ? 'background:#6eaf26 !important;color:#fff' : '' }}"
-                       href="{{ url('/' . Carbon::parse($assessmentsDate)->format('Y-m')) }}">{{ Carbon::parse($assessmentsDate)->format('Y F') }}</a>
+                        style="{{ Carbon::parse($month)->format('Y-m') == Carbon::parse($assessmentsDate)->format('Y-m') ? 'background:#6eaf26 !important;color:#fff' : '' }}"
+                        href="{{ url('/' . Carbon::parse($assessmentsDate)->format('Y-m')) }}">{{ Carbon::parse($assessmentsDate)->format('Y F') }}</a>
                 @endforeach
             </div>
         </div>
@@ -123,39 +123,39 @@
         <div class="card-body py-4">
             <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_datatable">
                 <thead>
-                <tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
-                    <th class="w-10px pe-2">
-                        <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
-                            <input class="form-check-input" hidden type="checkbox" data-kt-check="true"
-                                   data-kt-check-target="#kt_datatable .form-check-input" value="0"/>
-                        </div>
-                    </th>
-                    <th class="min-w-125px">Name</th>
-                    <th class="min-w-125px">Manager Name</th>
-                    <th class="min-w-125px">Assessment Title</th>
-                    <th class="min-w-125px">Rate</th>
-                </tr>
+                    <tr class="text-start text-gray-400 fw-bolder fs-7 text-uppercase gs-0">
+                        <th class="w-10px pe-2">
+                            <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
+                                <input class="form-check-input" hidden type="checkbox" data-kt-check="true"
+                                    data-kt-check-target="#kt_datatable .form-check-input" value="0" />
+                            </div>
+                        </th>
+                        <th class="min-w-125px">Name</th>
+                        <th class="min-w-125px">Manager Name</th>
+                        <th class="min-w-125px">Assessment Title</th>
+                        <th class="min-w-125px">Rate</th>
+                    </tr>
                 </thead>
                 <tbody class="text-gray-600 fw-bold render" id="kt_datatable_body">
-                @foreach ($RatedUsers as $rateUser)
-                    <tr id="row-{{ $rateUser->id }}">
-                        <td></td>
-                        <td class="name" data-field="name">
-                            {{ $rateUser?->user->name }}
-                        </td>
-                        <td class="name" data-field="name">
-                            {{ $rateUser?->assessment->manager->name }}
-                        </td>
+                    @foreach ($RatedUsers as $rateUser)
+                        <tr id="row-{{ $rateUser->id }}">
+                            <td></td>
+                            <td class="name" data-field="name">
+                                {{ $rateUser?->user->name }}
+                            </td>
+                            <td class="name" data-field="name">
+                                {{ $rateUser?->assessment->manager->name }}
+                            </td>
 
-                        <td class="name" data-field="name">
-                            {{ $rateUser?->assessment?->title }}
-                        </td>
-                        <td class="name" data-field="name">
-                            {{ $rateUser?->rate }} %
-                        </td>
+                            <td class="name" data-field="name">
+                                {{ $rateUser?->assessment?->title }}
+                            </td>
+                            <td class="name" data-field="name">
+                                {{ $rateUser?->rate }} %
+                            </td>
 
-                    </tr>
-                @endforeach
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
@@ -192,8 +192,8 @@
                         <div class="card-toolbar m-0">
                             <!--begin::Menu-->
                             <button type="button"
-                                    class="btn btn-clean btn-sm btn-icon btn-icon-primary btn-active-light-primary me-n3"
-                                    data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                                class="btn btn-clean btn-sm btn-icon btn-icon-primary btn-active-light-primary me-n3"
+                                data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
                                 {{-- <i class="ki-outline ki-element-plus fs-3 text-primary"></i> --}}
                             </button>
                             <!--begin::Menu 3-->
@@ -218,7 +218,7 @@
                             <!--begin::Number-->
                             <div class="fw-bold text-danger me-2">
                                 <a href="{{ route('admin.rate.details', ['assessment' => $firstQuarter->slug, 'startdate' => 'January 1', 'enddate' => 'March 31', 'userid' => $userId]) }}"
-                                   class="btn btn-primary btn-sm">Rate Details</a>
+                                    class="btn btn-primary btn-sm">Rate Details</a>
                             </div>
                             <!--end::Number-->
                         </div>
@@ -236,8 +236,7 @@
 
 @endif
 
-@if(auth()->user()->AssessmentManager()->count() >= 1)
-
+@if (auth()->user()->AssessmentManager()->count() >= 1)
     <div class="card mt-8">
         <!--begin::Card header-->
 
@@ -321,20 +320,14 @@
 
     // Call the updateChart function with the assessmentData
     updateChart(assessmentData);
-
-
-
-
 </script>
 
 @push('js')
     <script>
-        $(document).ready(function () {
-
-
+        $(document).ready(function() {
             var options = {
                 series: [{
-                    name: "Desktops",
+                    name: "Rate",
                     data: @json($empRates)
                 }],
                 chart: {
